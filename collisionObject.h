0ff0f129d9d3edf6ce97108b62b70f159d2d12fe
Logan Huston <shadows7590@gmail.com>
2022-11-10 15:22:31 -0700

    Added collisionObject and object h files

---
+++ collisionObject.h
@@ -0,0 +1,15 @@
+
+
+#pragma once
+
+/*********************************************
+ * Collision Object
+ * A fragment that is spawned in front of the spaceship.
+ *********************************************/
+class CollisionObject
+{
+public:
+
+private:
+
+};
