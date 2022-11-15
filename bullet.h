7c455e0684e904fdda402368fd7ac823529051c3
Logan Huston <shadows7590@gmail.com>
2022-11-15 15:32:34 -0700

    Added header file for Bullet Test

--- bullet.h
+++ bullet.h
@@ -4,6 +4,8 @@
 
 #include "fragment.h"
 
+class BulletTest;
+
 /*********************************************
  * Bullet
  * A fragment that is spawned in front of the spaceship.
@@ -11,9 +13,7 @@
 class Bullet : public Fragment
 {
 public:
+   friend BulletTest;
    Bullet() : Bullet(Position(), Velocity(), Angle()) {};
    Bullet(Position pos, Velocity vel, Angle angle) : Fragment(pos, vel, 0.0) {};
-   
-private:
-
 };
