55662196d3551658366e9e333c076d7212563460
Logan Huston <shadows7590@gmail.com>
2022-11-15 16:14:55 -0700

    Added angle

--- angle.h
+++ angle.h
@@ -1,17 +1,27 @@
-
-
+/***********************************************************************
+ * Header File:
+ *    Angle : Represents an angle in a 2D space
+ * Author:
+ *    Eddie McConkie, Logan Huston
+ * Summary:
+ * Calculates angle with radians and can accept both radians and degrees
+ ************************************************************************/
 #pragma once
 
-/*********************************************
- * Angle
- * 
- *********************************************/
+ /***********************************
+ * ANGLE
+ * The angle class
+ *************************************/
 class Angle
 {
 public:
-   Angle() {};
-
+   Angle() : radAngle(0.0) {}
+   Angle(double radAngle) : radAngle(radAngle) {}
+   double getRadAngle() const { return radAngle; }
+   double getDegAngle() const;
+   Angle& operator+=(const double rhs);
+   Angle& operator-=(const double rhs);
 private:
-
+   double radAngle;
+   void normalize();
 };
-
