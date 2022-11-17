beb072563e2170f800f4d13b8a159bd5c8452070
Logan Huston <shadows7590@gmail.com>
2022-11-17 15:45:39 -0700

    bullet test filled out

--- spaceshipTest/testBullet.h
+++ spaceshipTest/testBullet.h
@@ -24,6 +24,7 @@ public:
    virtual double getRadius() const { assert(false);  return 0.0; };
 };
 
+// Stub for a ship that is pointed up, and goes nowhere.
 class stubDirectionUp : public dummyCollisionObject {
 public:
    double getAngle() const { return 0.0; };
@@ -31,6 +32,7 @@ public:
    Velocity getVelocity() const { return Velocity(0.0, 0.0); }
 };
 
+// Stub for a ship that is pointed up, and has a velocity of 10,000 m/s.
 class stubMoveUp1000 : public dummyCollisionObject {
 public:
    double getAngle() const { return 0.0; }
@@ -44,44 +46,65 @@ public:
       Position().setZoom(1000.0);
       shootStationary();
       shootMoving();
-      deleteExpire();
-      deleteCollision();
+      expireTimer();
+      expireCollision();
    }
 private:
    void shootStationary() const
    {
       // Setup
       stubDirectionUp ship;
+      Position spawnPos;
+      spawnPos.addPixelsY(19.0);
       
       // Exercise
-      Bullet test(ship.getPosition(), ship.getVelocity(), ship.getAngle());
+      Bullet test(ship.getPosition() + spawnPos, ship.getVelocity(), ship.getAngle());
       // Verify
-      assert(test.pos.getMetersX() == 0.0);
+      assert(closeEnough(test.pos.getMetersX(), 0.0, 0.1));
+      assert(closeEnough(test.pos.getMetersY(), 19.0, 0.1));
+      assert(closeEnough(test.angle, 0.0, 0.1));
+      assert(closeEnough(test.vel.getMetersX(), 0.0, 0.1));
+      assert(closeEnough(test.vel.getMetersY(), 9000.0, 1.0));
    }  // Teardown
    
    void shootMoving() const 
    {
       // Setup
       stubMoveUp1000 ship;
+      Position spawnPos;
+      spawnPos.addPixelsY(19.0);
+
       // Exercise
-      Bullet test;
+      Bullet test(ship.getPosition() + spawnPos, ship.getVelocity(), ship.getAngle());
       // Verify
+      assert(closeEnough(test.pos.getMetersX(), 0.0, 0.1));
+      assert(closeEnough(test.pos.getMetersY(), 19.0, 0.1));
+      assert(closeEnough(test.angle, 0.0, 0.1));
+      assert(closeEnough(test.vel.getMetersX(), 0.0, 0.1));
+      assert(closeEnough(test.vel.getMetersY(), 10000.0, 1.0));
    }  // Teardown
 
-   void deleteExpire() const
+   void expireTimer() const
    {
       // Setup
-      Bullet test;
+      Bullet test(Position(10.0, 10.0), Velocity(9000.0, 0.0), Angle());
+      test.lifetime = 0;
       // Exercise
+      bool expired = test.isExpired();
       // Verify
+      assert(expired == true);
+      assert(test.lifetime <= 0);
    }  // Teardown
 
-   void deleteCollision() const
+   void expireCollision() const
    {
       // Setup
-      Bullet test;
-      Bullet test2;
+      Bullet test(Position(0.0, 10.0), Velocity(0.0, 0.0), Angle(0.0));
+      Bullet test2(Position(0.0, 10.0), Velocity(0.0, 0.0), Angle(0.0));
       // Exercise
+      bool expired = test.isExpired();
       // Verify
+      assert(expired == true);
+      assert(test.lifetime > 0);
    }  // Teardown
 };
